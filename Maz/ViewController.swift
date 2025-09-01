3c45ebe77ce3a547e8c05ff5e7e670351f7d71cc
TheMAZ2-png <csm012004@outlook.com>
2025-09-01 16:45:25 -0400

    Initial Commit

---
+++ Maz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maz
+//
+//  Created by Chris Mazurek on 9/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MazTests/MazTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazTests.swift
+//  MazTests
+//
+//  Created by Chris Mazurek on 9/1/25.
+//
+
+import Testing
+@testable import Maz
+
+struct MazTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
